2cc96e0e6f937e8f29b01f891374059cc54a56fa
Ewa Smigaj <ewa.smigaj1@gmail.com>
2025-09-17 16:10:53 +0200

    gaming + modularization

---
+++ modules/nixos/gaming.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [nvidia-vaapi-driver];
+  };
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup-qt 
+    lutris 
+    bottles 
+    heroic 
+  ];
+
+}
